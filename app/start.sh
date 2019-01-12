994fa13b79cc3b33195b0d853a389170102fbff4
Jan VdA <jan.vandenaudenaerde@gmail.com>
2019-01-12 14:38:57 +0100

    added environment variable  to enable/disable remote control

--- app/start.sh
+++ app/start.sh
@@ -8,8 +8,15 @@ fi
 # Handle Kodi userdata folder
 mkdir /data/kodi >/dev/null 2>&1 || true && rm -rf /root/.kodi && ln -s /data/kodi /root/.kodi
 
-# Copy advancedsettings.xml to proper folder.
-mkdir /data/kodi/userdata >/dev/null 2>&1 || true && cp /usr/src/app/advancedsettings.xml /data/kodi/userdata
+# env variable $REMOTE_CONTROL can be used to enable remote control 
+# see https://kodi.wiki/view/Web_interface
+if [[ "$REMOTE_CONTROL" = "1" ]]; then
+  # Copy advancedsettings.xml to proper folder.
+  mkdir /data/kodi/userdata >/dev/null 2>&1 || true && cp /usr/src/app/advancedsettings.xml /data/kodi/userdata
+else
+  # Remove advancedsettings.xml 
+  mkdir /data/kodi/userdata >/dev/null 2>&1 || true && rm -rf /data/kodi/userdata/advancedsettings.xml
+if
 
 while true; do
     DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket /usr/bin/kodi-standalone
