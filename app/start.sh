59b36dfa86119978f2203969dca720ce2009485d
Jan VdA <jan.vandenaudenaerde@gmail.com>
2019-01-12 14:23:02 +0100

    deployed advancedsettings.xml to activate remote control

--- app/start.sh
+++ app/start.sh
@@ -8,6 +8,9 @@ fi
 # Handle Kodi userdata folder
 mkdir /data/kodi >/dev/null 2>&1 || true && rm -rf /root/.kodi && ln -s /data/kodi /root/.kodi
 
+# Copy advancedsettings.xml to proper folder.
+mkdir /data/kodi/userdata >/dev/null 2>&1 || true && cp /usr/src/app/advancedsettings.xml /data/kodi/userdata
+
 while true; do
     DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket /usr/bin/kodi-standalone
 done
